89ac939cc64bbbb08c7020f2d0705b2d2e7a8875
dangkhoa <dangkhoa2250@gmail.com>
2023-03-29 13:24:50 +0900

    update ls8

--- ls7/ls7.js
+++
@@ -1,5 +0,0 @@
-console.log("hello khoa")
-let a = 5;
-let b = "5";
-console.log(`compare ${a} and ${b} by == : ${a==b}`)
-console.log(`compare ${a} and ${b} by === : ${a===b}`)
---
+++ ls8/ls8.js
@@ -0,0 +1,10 @@
+for (let i = 0; i < 10; i++){
+    console.log(`check i value: ${i}`)
+}
+console.log("==================")
+let arr = ["mu", "mancity", "chelsea", "liver"]
+for(let i = 0; i < arr.length; i++){
+    if (arr[i].length == 2){
+        console.log(`${arr[i]}`)
+    }
+}
